9da3a4c37982b4e92a2084f820681d654418a635
your name <subhankardasmohanty@gmail.com>
2026-06-25 06:33:23 +0530

    Add LeetCode 29: Divide Two Integers

---
+++ Leetcode/src/Math/Q29_Divide_Two_Integers.java
@@ -0,0 +1,11 @@
+package Math;
+
+public class Q29_Divide_Two_Integers {
+
+    public int divide(int dividend, int divisor) {
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+        return dividend/divisor;
+    }
+}
