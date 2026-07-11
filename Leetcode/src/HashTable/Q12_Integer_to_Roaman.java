94d688454b3b5d4432114c48ffca35eafbe29f0a
your name <subhankardasmohanty@gmail.com>
2026-07-11 10:35:31 +0530

    Add leetcode Q12

---
+++ Leetcode/src/HashTable/Q12_Integer_to_Roaman.java
@@ -0,0 +1,24 @@
+package HashTable;
+
+public class Q12_Integer_to_Roaman {
+
+    public String intToRoman(int num) {
+
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M", "CM", "D",  "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0;i<values.length;i++){
+            if(num==0){
+                break;
+            }
+            while(num>=values[i]){
+                sb.append(symbols[i]);
+                num-=values[i];
+            }
+        }
+
+        return sb.toString();
+    }
+}
