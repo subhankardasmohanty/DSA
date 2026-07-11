07ec03898053ebcdb5b6b2c436055d0d9e4f9b51
your name <subhankardasmohanty@gmail.com>
2026-07-11 10:39:00 +0530

    Add codeforce Q791A

---
+++ CodeForces/src/ProblemSet/Q791A_Bear_and_Big_Brother.java
@@ -0,0 +1,23 @@
+package ProblemSet;
+
+import java.util.Scanner
+
+public class Q791A_Bear_and_Big_Brother {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int years = 0;
+
+        while (a <= b) {
+            a *= 3;
+            b *= 2;
+            years++;
+        }
+
+        System.out.println(years);
+    }
+}
