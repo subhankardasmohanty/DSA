8cf42860290b80620f723d592637965c20ae2b5f
your name <subhankardasmohanty@gmail.com>
2026-07-12 11:46:24 +0530

    Add codeforce Q617A

---
+++ CodeForces/src/ProblemSet/Q617A_Elephant.java
@@ -0,0 +1,14 @@
+package ProblemSet;
+
+import java.util.*;
+
+public class Q617A_Elephant {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+
+        System.out.println((x + 4) / 5);
+    }
+}
