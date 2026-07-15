a11fcb8a86021d3aaa0dd778655153526ecf5a18
your name <subhankardasmohanty@gmail.com>
2026-07-15 12:03:19 +0530

    Add leetcode Q118A

---
+++ CodeForces/src/ProblemSet/Q118A_String_Task.java
@@ -0,0 +1,28 @@
+package ProblemSet;
+
+import java.util.*;
+
+public class Q118A_String_Task {
+
+    import java.util.*;
+
+    public class Main {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            String s = sc.next();
+
+            StringBuilder ans = new StringBuilder();
+
+            for (int i = 0; i < s.length(); i++) {
+                char ch = Character.toLowerCase(s.charAt(i));
+
+                if (ch != 'a' && ch != 'e' && ch != 'i' &&
+                        ch != 'o' && ch != 'u' && ch != 'y') {
+                    ans.append('.').append(ch);
+                }
+            }
+
+            System.out.print(ans);
+        }
+    }
+}
