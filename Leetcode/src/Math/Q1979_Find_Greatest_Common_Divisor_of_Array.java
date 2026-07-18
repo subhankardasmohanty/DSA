b78887666bf606a835d0f20a88ce81385fdd5607
your name <subhankardasmohanty@gmail.com>
2026-07-18 19:52:03 +0530

    Add leetcode Q1979

---
+++ Leetcode/src/Math/Q1979_Find_Greatest_Common_Divisor_of_Array.java
@@ -0,0 +1,25 @@
+package Math;
+
+public class Q1979_Find_Greatest_Common_Divisor_of_Array {
+
+    public int findGCD(int[] nums) {
+        int min = nums[0];
+        int max = nums[0];
+
+        for (int num : nums) {
+            if (num < min) min = num;
+            if (num > max) max = num;
+        }
+
+        return gcd(min, max);
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
