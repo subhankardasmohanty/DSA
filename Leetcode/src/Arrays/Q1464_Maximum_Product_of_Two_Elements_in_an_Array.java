caf68c0d5db2defd690daaacd1ed5d725ea0d0c7
your name <subhankardasmohanty@gmail.com>
2026-07-27 09:31:31 +0530

    Add leetcode Q1464

---
+++ Leetcode/src/Arrays/Q1464_Maximum_Product_of_Two_Elements_in_an_Array.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class Q1464_Maximum_Product_of_Two_Elements_in_an_Array {
+
+    public int maxProduct(int[] nums) {
+        int max1 = 0, max2 = 0;
+
+        for (int num : nums) {
+            if (num > max1) {
+                max2 = max1;
+                max1 = num;
+            } else if (num > max2) {
+                max2 = num;
+            }
+        }
+
+        return (max1 - 1) * (max2 - 1);
+    }
+}
