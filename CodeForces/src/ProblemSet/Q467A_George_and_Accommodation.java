f9cc8d0250a99ad1ca2d9a3b53756f3c4cbb914c
your name <subhankardasmohanty@gmail.com>
2026-07-28 08:13:12 +0530

    Add codeforce Q467A

---
+++ CodeForces/src/ProblemSet/Q467A_George_and_Accommodation.java
@@ -0,0 +1,24 @@
+package ProblemSet;
+
+import java.util.*;
+
+public class Q467A_George_and_Accommodation {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            int p = sc.nextInt();
+            int q = sc.nextInt();
+
+            if (q - p >= 2) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
