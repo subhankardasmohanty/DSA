fd8e37a5324247dc012697bdc7b59a9bad564862
your name <subhankardasmohanty@gmail.com>
2026-08-06 16:46:13 +0530

    Add codeforce Q318A

---
+++ CodeForces/src/ProblemSet/Q318A_Even_Odds.java
@@ -0,0 +1,21 @@
+package ProblemSet;
+
+import java.util.Scanner;
+
+public class Q318A_Even_Odds {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+        long k = sc.nextLong();
+
+        long oddCount = (n + 1) / 2;
+
+        if (k <= oddCount) {
+            System.out.println(2 * k - 1);
+        } else {
+            System.out.println(2 * (k - oddCount));
+        }
+    }
+}
